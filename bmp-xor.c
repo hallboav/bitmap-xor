cb08536cb3cd184aab9ae01fc496b7ab6dd914d4
Hallison Boaventura <hallisonboaventura@gmail.com>
2016-05-13 00:35:44 -0300

    Lendo informações sobre o arquivo

--- bmp-xor.c
+++ bmp-xor.c
@@ -6,6 +6,13 @@
 #define BITMAP_INFO_HEADER_SIZE  40
 #define BITMAP_INFO_HEADER_START 14
 
+typedef struct {
+    uint16_t type;
+    uint32_t size;
+    uint32_t reserved;
+    uint32_t offset_bits;
+} bitmap_file_header_t;
+
 int32_t main(int32_t argc, int8_t *argv[])
 {
     if (argc != 4) {
@@ -34,6 +41,15 @@ int32_t main(int32_t argc, int8_t *argv[])
         fclose(fin);
         return 3;
     }
+
+    bitmap_file_header_t file_header;
+    memset(&file_header, 0, sizeof(bitmap_file_header_t));
+
+    fseek(fin, 0, SEEK_SET);
+    fread(&file_header.type,        sizeof(uint16_t), 1, fin);
+    fread(&file_header.size,        sizeof(uint32_t), 1, fin);
+    fread(&file_header.reserved,    sizeof(uint32_t), 1, fin);
+    fread(&file_header.offset_bits, sizeof(uint32_t), 1, fin);
 
     fclose(fin);
     return 0;
